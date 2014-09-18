033cf4e39f5e82aa214dbff21cd1b7ab2fb15b26
telly <telly@bskyb.com>
2014-09-18 06:17:55 +0100

    BM: Initial commit with scaffolding

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('meanFawApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
